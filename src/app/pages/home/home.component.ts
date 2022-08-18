bd2e5f650464efeab55966c5196cdb1f51b202e1
naimur <naimur202163@gmail.com>
2022-08-18 11:43:02 +0600

    working to fetch data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,27 +1,18 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, Injectable, OnInit } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { UserDataService } from 'src/services/user-data.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-@Injectable({
-  providedIn: 'root',
-})
 export class HomeComponent implements OnInit {
-  constructor(private http: HttpClient) {}
   users: any;
-
-  ngOnInit(): void {
-    this.getData();
+  constructor(private userData: UserDataService) {
+    userData.users().subscribe((data) => {
+      this.users = data;
+      console.warn(this.users);
+    });
   }
 
-  getData() {
-    return this.http
-      .get('https://jsonplaceholder.typicode.com/users')
-      .subscribe((res) => {
-        console.log('res', res);
-      });
-  }
+  ngOnInit(): void {}
 }
